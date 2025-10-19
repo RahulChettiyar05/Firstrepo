af73ea62e4c8085fdc6688789f09785d7a96c6f9
RahulChettiyar05 <rahulgopu5@gmail.com>
2025-10-19 10:20:15 +0530

    Added python addition

--- add.py
+++ add.py
@@ -1,6 +1,7 @@
 print("Hello world")
 
-a = input(int("Enter a number : "))
-b = input(int("Enter a number : "))
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+
 c = a+b
 print(f"The addition is {c}")
